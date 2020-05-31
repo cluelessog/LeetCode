c68dbdbcdf5fffb2a37de1e549069dc431379cb8
Sourabh Kumar <sourabhkumar0308@gmail.com>
2020-05-31 15:07:40 +0530

    Dynamic Programming

---
+++ MayChallenge/minDistance.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    int minDistance(string word1, string word2) {
+        int n = word1.size();
+        int m = word2.size();
+        if(n == 0 and m==0) return 0;
+        int dp[n+1][m+1];
+        for(int i = 0 ; i <= n; i++)
+        {
+            for(int j = 0; j <= m; j++)
+            {
+                if(i == 0)
+                {
+                    dp[i][j] = j;
+                }
+                else if(j == 0)
+                {
+                    dp[i][j] = i;
+                }
+                else if(word1[i-1] == word2[j-1])
+                {
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else
+                {
+                    dp[i][j] = 1 + min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]));
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
