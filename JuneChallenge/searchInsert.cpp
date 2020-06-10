8dcc495cf5ff7f9488049c42b6f1059de5cb0a66
Sourabh Kumar <sourabhkumar0308@gmail.com>
2020-06-10 19:03:35 +0530

    binary search

---
+++ JuneChallenge/searchInsert.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        int mid = 0;
+        while(l<=r)
+        {
+            mid = l + (r-l)/2;
+            if(nums[mid] == target)
+            {
+                return mid;
+            }
+            if(nums[mid] < target)
+            {
+                l = mid+1;
+            }
+            else
+            {
+                r = mid-1;
+            }
+        }
+        return l;
+    }
+};
